fdd34dd7c7c5f8d76e5f608aebfffe61c5e50910
valeis <vb23745@gmail.com>
2023-09-20 11:11:11 +0300

    some minor changes

--- pkg/database/postgres/postgres.go
+++ pkg/database/postgres/postgres.go
@@ -14,19 +14,14 @@ var Database *gorm.DB
 func Connect() {
 	dbConfig := config.Cfg.Database
 	var err error
-
-	dsn := fmt.Sprintf(`host=%s
-	dbname=%s
-	user=%s
-	password=%s
-	port=%d
-	sslmode=disable`,
-		dbConfig.Host,
-		dbConfig.DBName,
-		dbConfig.User,
-		dbConfig.Password,
-		dbConfig.Port,
-	)
+	log.Printf("%s", dbConfig);
+	dsn := fmt.Sprintf("host=%s dbname=%s user=%s password=%s port=%d sslmode=disable",
+    dbConfig.Host,
+    dbConfig.DBName,
+    dbConfig.User,
+    dbConfig.Password,
+    dbConfig.Port,
+)
 
 	Database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
