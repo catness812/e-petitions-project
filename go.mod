fadf0691961f943eff82fca00b0d359bac1dbb99
valeis <vb23745@gmail.com>
2023-09-20 10:44:35 +0300

    initial config

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/catness812/e-petitions-project
 
 go 1.21.0
+
+require (
+	gopkg.in/yaml.v2 v2.4.0
+	gorm.io/driver/postgres v1.5.2
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.3.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	gorm.io/gorm v1.25.4 // indirect
+)
---
+++ pkg/database/postgres/postgres.go
@@ -0,0 +1,38 @@
+package postgres
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/catness812/e-petitions-project/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var Database *gorm.DB
+
+func Connect() {
+	dbConfig := config.Cfg.Database
+	var err error
+
+	dsn := fmt.Sprintf(`host=%s
+	dbname=%s
+	user=%s
+	password=%s
+	port=%d
+	sslmode=disable`,
+		dbConfig.Host,
+		dbConfig.DBName,
+		dbConfig.User,
+		dbConfig.Password,
+		dbConfig.Port,
+	)
+
+	Database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Panic(err)
+	} else {
+		log.Println("Successfully connected to the Postgres database")
+	}
+}
