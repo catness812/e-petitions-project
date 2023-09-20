b4de86c94521382436af4827cb75fc5847a2e930
Masha003 <colta.maha@gmail.com>
2023-09-20 16:51:36 +0300

    Created config file.

--- gateway/config/config.go
+++ gateway/config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	PetitionPort int `yaml:"petition_port"`
+	UserPort     int `yaml:"user_port"`
+	SecurityPort int `yaml:"security_port"`
+	HttpPort     int `yaml:"http_port"`
+}
+
+func LoadConfig() *Config {
+	var cfg *Config
+	data, err := os.ReadFile("config/config.yml")
+	if err != nil {
+		log.Fatalf("Failed to read configuration file: %v", err)
+	}
+
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		log.Fatalf("Failed to unmarshal YAML data to config: %v", err)
+	}
+	return cfg
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/catness812/e-petitions-project
 
 go 1.21.0
+
+require gopkg.in/yaml.v3 v3.0.1
