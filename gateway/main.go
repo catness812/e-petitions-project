9095e93278968496492481fd6e7f3a3a62014cd8
Masha003 <colta.maha@gmail.com>
2023-09-20 17:43:08 +0300

    Load config in main.

--- gateway/main.go
+++ gateway/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+
+	"github.com/catness812/e-petitions-project/gateway/config"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+	r := gin.Default()
+
+	// authsvc.RegisterRoutes(r, &c)
+	// usersvc.RegisterRoutes(r, &c)
+	// adminsvc.RegisterRoutes(r, &c)
+
+	r.Run(":1337")
 
 }
