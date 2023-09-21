5a5ebc0f32a9f2b438f0467b84eab1b4bc67e51a
maria-afteni <marianaafteni2003@gmail.com>
2023-09-21 12:57:49 +0300

    Added Update and Delete functions

--- internal/service/petition_service.go
+++ internal/service/petition_service.go
@@ -3,6 +3,8 @@ package service
 import (
 	"github.com/catness812/e-petitions-project/internal/models"
 	"github.com/catness812/e-petitions-project/internal/repository"
+	"log"
+	"gorm.io/gorm"
 )
 
 func CreateNew(petition models.Petition) (uint, error) {
@@ -11,4 +13,33 @@ func CreateNew(petition models.Petition) (uint, error) {
 	} else {
 		return petition.ID, nil
 	}
+}
+
+func Update(db *gorm.DB, petition models.Petition) error {
+	var existingPetition models.Petition
+	if err := db.First(&existingPetition, petition.ID).Error; err != nil {
+		log.Fatalf("Failed to find petition: %d\n", err)
+		return err
+	}
+
+	existingPetition.Title = petition.Title
+	existingPetition.Category = petition.Category
+	existingPetition.Description = petition.Description
+	existingPetition.Image = petition.Image
+
+	if err := db.Save(&existingPetition).Error; err != nil {
+		log.Fatalf("Failed to save updated petition: %d\n", err)
+		return err
+	}
+
+	return nil
+}
+
+func Delete(db *gorm.DB, petition models.Petition) error {
+	err := db.Delete(petition.ID).Error
+	if err != nil {
+		log.Fatalf("Failed to delete petition: %d\n", err)
+		return err
+	}
+	return nil
 }
