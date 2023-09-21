eff1398aaac8b782938c9bea97896646d8dda67b
MacriiDanu <121299815+auperman-lab@users.noreply.github.com>
2023-09-21 11:44:27 +0300

    change config from int to string, create user client and router , also add user service and controller files

--- gateway/http/user/client.go
+++ gateway/http/user/client.go
@@ -1,1 +1,22 @@
 package user
+
+import (
+	"github.com/catness812/e-petitions-project/gateway/config"
+	"github.com/catness812/e-petitions-project/gateway/http/user/pb"
+	"google.golang.org/grpc"
+	"log"
+)
+
+type ServiceClient struct {
+	Client pb.UserControllerClient
+}
+
+func InitUserControllerClient(c *config.Config) pb.UserControllerClient {
+	cc, err := grpc.Dial(c.UserPort, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal("Could not connect:", err)
+	}
+
+	return pb.NewUserControllerClient(cc)
+}
--- gateway/main.go
+++ gateway/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/catness812/e-petitions-project/gateway/config"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	c := config.LoadConfig()
+
+	r := gin.Default()
+
+	r.Run(":8080")
 
 }
