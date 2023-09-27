064746a064cd072787f05e5fd44ff9367e5e2d01
auperman-lab <naauperman@gmail.com>
2023-09-27 18:09:46 +0300

    add all structure for ewquest response for security and user, tested for user_service

--- gateway/internal/security/security_service.go
+++ gateway/internal/security/security_service.go
@@ -1,8 +1,10 @@
 package security
 
+import "github.com/catness812/e-petitions-project/gateway/model"
+
 type ISecurityService interface {
-	Login()
-	Refresh()
+	Login(loginUser model.UserCredentials) (model.Tokens, error)
+	Refresh(token string) (model.Tokens, error)
 }
 
 func NewSecurityService(repo ISecurityRepository) (ISecurityService, error) {
@@ -15,10 +17,11 @@ type securityService struct {
 	repo ISecurityRepository
 }
 
-func (svc *securityService) Login() {
+func (svc *securityService) Login(loginUser model.UserCredentials) (model.Tokens, error) {
+	return svc.repo.Login(loginUser)
 
 }
 
-func (svc *securityService) Refresh() {
-
+func (svc *securityService) Refresh(token string) (model.Tokens, error) {
+	return svc.repo.Refresh(token)
 }
--- gateway/internal/user/user_repository.go
+++ gateway/internal/user/user_repository.go
@@ -1,15 +1,19 @@
 package user
 
 import (
+	"context"
+	"errors"
 	"github.com/catness812/e-petitions-project/gateway/internal/config"
 	"github.com/catness812/e-petitions-project/gateway/internal/user/pb"
+	"github.com/catness812/e-petitions-project/gateway/model"
 )
 
 type IUserRepository interface {
-	Get()
-	Delete()
-	Create()
-	Update()
+	Get(email string) (model.User, error)
+	Delete(email string) (string, error)
+	Create(createUser model.UserCredentials) error
+	Update(createUser model.UserCredentials) error
+	AddAdmin()
 }
 
 func NewUserRepository(c config.Config, client pb.UserControllerClient) (IUserRepository, error) {
@@ -27,17 +31,65 @@ type userRepository struct {
 	client pb.UserControllerClient
 }
 
-func (svc *userRepository) Get() {
+func (repo *userRepository) Get(email string) (model.User, error) {
+	res, err := repo.client.GetUserByEmail(context.Background(), &pb.GetUserByEmailRequest{
+		Email: email,
+	})
+	if err != nil {
+		return model.User{}, err
+	}
+	var user model.User
+	user.UserCredentials.Email = res.Email
+	user.UserCredentials.Password = res.Password
+	user.Id = res.Id
+	user.Role = res.Role
+
+	return user, nil
 }
 
-func (svc *userRepository) Delete() {
+func (repo *userRepository) Delete(email string) (string, error) {
+	res, err := repo.client.DeleteUser(context.Background(), &pb.DeleteUserRequest{
+		Email: email,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	if res != nil && res.Value != "" {
+		return res.Value, nil
+	}
+
+	return "", errors.New("DeleteUser response is empty")
+}
+
+func (repo *userRepository) Create(createUser model.UserCredentials) error {
+
+	_, err := repo.client.CreateUser(context.Background(), &pb.UserRequest{
+		Email:    createUser.Email,
+		Password: createUser.Password,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 
 }
 
-func (svc *userRepository) Create() {
+func (repo *userRepository) Update(createUser model.UserCredentials) error {
+	_, err := repo.client.UpdateUser(context.Background(), &pb.UserRequest{
+		Email:    createUser.Email,
+		Password: createUser.Password,
+	})
 
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (svc *userRepository) Update() {
+func (repo *userRepository) AddAdmin() {
 
 }
