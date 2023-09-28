1096b471c5b758c3f8d194f2860ad4e3b5efa4d9
Adrian Popov <qwasuss@gmail.com>
2023-09-28 08:52:22 +0300

    move config

--- mail_service/internal/controllers/mail_controllers.go
+++ mail_service/internal/controllers/mail_controllers.go
@@ -9,31 +9,55 @@ import (
 
 type Consumer struct {
 	channel *amqp.Channel
-	name    string
 }
 
-func NewConsumer(channel *amqp.Channel, name string) *Consumer {
+func NewConsumer(channel *amqp.Channel) *Consumer {
 	return &Consumer{
 		channel: channel,
-		name:    name,
 	}
 }
 
-func (c *Consumer) ConsumeAndSend() {
+func (c *Consumer) ConfirmationMail(name string) {
 	msgs, err := c.channel.Consume(
-		c.name, // queue
-		"",     // consumer
-		true,   // auto ack
-		false,  // exclusive
-		false,  // no local
-		false,  // no wait
-		nil,    //args
+		name,  // queue
+		"",    // consumer
+		true,  // auto ack
+		false, // exclusive
+		false, // no local
+		false, // no wait
+		nil,   //args
 	)
 	if err != nil {
 		panic(err)
 	}
 
-	log.Printf("Consumer %s started", c.name)
+	log.Printf("Consumer %s started", name)
+	// print consumed messages from queue
+	forever := make(chan bool)
+	go func() {
+		for msg := range msgs {
+			service.SendMail(string(msg.Body))
+		}
+	}()
+
+	<-forever
+}
+
+func (c *Consumer) NotificationMail(name string) {
+	msgs, err := c.channel.Consume(
+		name,  // queue
+		"",    // consumer
+		true,  // auto ack
+		false, // exclusive
+		false, // no local
+		false, // no wait
+		nil,   //args
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Consumer %s started", name)
 	// print consumed messages from queue
 	forever := make(chan bool)
 	go func() {
--- mail_service/main.go
+++ mail_service/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/catness812/e-petitions-project/mail_service/internal/config"
 	"github.com/catness812/e-petitions-project/mail_service/internal/controllers"
 	"github.com/catness812/e-petitions-project/mail_service/pkg/rabbitMQ"
 
@@ -18,15 +19,20 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to declare queue: %v", err)
 	}
-	cons := controllers.NewConsumer(ch, q.Name)
-	cons.ConsumeAndSend()
+	cons := controllers.NewConsumer(ch)
+	cons.ConfirmationMail(q.Name)
 }
 
 func init() {
-	err := godotenv.Load("mail_service/.env")
+	err := godotenv.Load("./mail_service/.env")
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		err = godotenv.Load("../mail_service/.env")
+		if err != nil {
+			log.Fatal("failed to load .env")
+		}
 	}
 
-	ch = rabbitMQ.ConnectAMQPDataBase(os.Getenv("RABBITMQ_USER"), os.Getenv("RABBITMQ_PASS"), "localhost", "5672")
+	config.LoadConfig()
+
+	ch = rabbitMQ.ConnectAMQPDataBase(os.Getenv("RABBITMQ_USER"), os.Getenv("RABBITMQ_PASS"), config.Cfg.Rabbit.Host, config.Cfg.Rabbit.Port)
 }
