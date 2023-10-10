714a9167a86d06ba0b05553ec209625703d8a5ca
GrumpyCatYo <linkolnmax24@gmail.com>
2023-10-10 17:44:08 +0300

    UPDATE GATEWAY

--- gateway/main.go
+++ gateway/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"log"
-
 	"github.com/catness812/e-petitions-project/gateway/internal/config"
 	"github.com/catness812/e-petitions-project/gateway/internal/petition"
 	"github.com/catness812/e-petitions-project/gateway/internal/security"
 	"github.com/catness812/e-petitions-project/gateway/internal/user"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
@@ -15,6 +15,14 @@ func main() {
 	rbacCfg := config.LoadConfigRBAC()
 
 	r := gin.Default()
+
+	co := cors.DefaultConfig()
+	co.AllowOrigins = []string{"http://localhost:5173"}
+	co.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	co.AllowHeaders = []string{"Content-Type", "*"}
+	co.AllowHeaders = []string{"Allow-Control-Allow-Origin", "*"}
+
+	r.Use(cors.New(co))
 
 	user.RegisterUserRoutes(r, &config.Cfg, rbacCfg)
 	petition.RegisterPetitionRoutes(r, &config.Cfg)
