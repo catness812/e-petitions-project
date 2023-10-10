2b0dc0093d45395ae984268ae34d3d01400d8712
Adrian Popov <qwasuss@gmail.com>
2023-10-10 17:45:50 +0300

    add recover func after 30 sec

--- mail_service/pkg/rabbitMQ/rabbitMQ.go
+++ mail_service/pkg/rabbitMQ/rabbitMQ.go
@@ -1,20 +1,17 @@
 package rabbitMQ
 
 import (
-	"github.com/gookit/slog"
 	"github.com/streadway/amqp"
 )
 
 func ConnectAMQPDataBase(user string, pass string, host string, port string) *amqp.Channel {
 	conn, err := amqp.Dial("amqp://" + user + ":" + pass + "@" + host + ":" + port + "/")
 	if err != nil {
-		slog.Fatalf("cant start RabbitMQ: %v", err)
 		panic(err)
 	}
 
 	ch, err := conn.Channel()
 	if err != nil {
-		slog.Fatalf("cant start channal: %v", err)
 		panic(err)
 	}
 
