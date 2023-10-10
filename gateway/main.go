971df44934098cc9e937bd9e44e0a1589604172b
auperman-lab <naauperman@gmail.com>
2023-10-10 22:58:13 +0300

    corrected responses, add CORS, tested and added examples for endpoints

--- gateway/main.go
+++ gateway/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"log"
+	"time"
 
 	"github.com/catness812/e-petitions-project/gateway/internal/config"
 	"github.com/catness812/e-petitions-project/gateway/internal/petition"
@@ -15,6 +17,15 @@ func main() {
 	rbacCfg := config.LoadConfigRBAC()
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:1337"},
+		AllowMethods:     []string{"PUT", "GET", "POST", "DELETE"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length", "*"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	user.RegisterUserRoutes(r, &config.Cfg, rbacCfg)
 	petition.RegisterPetitionRoutes(r, &config.Cfg)
