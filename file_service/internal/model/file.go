369387b2d2fda2f4300757cffc038a9cc5015e3f
ktruedat <crinnic77@gmail.com>
2023-11-04 21:04:02 +0200

    Merged latest master changes

--- file_service/internal/model/file.go
+++ file_service/internal/model/file.go
@@ -9,6 +9,15 @@ type File struct {
 	Chunks   []Chunk
 }
 
+type User struct {
+	Id         uint32 `gorm:"primaryKey;autoIncrement:true;unique"`
+	Email      string `gorm:"not null;unique" json:"email"`
+	Password   string `gorm:"not null" json:"password"`
+	Role       string `json:"role"`
+	HasAccount bool   `json:"HasAccount"`
+	Files      []File `gorm:"many2many:user_files"`
+}
+
 type Chunk struct {
 	ID             uint   `gorm:"primaryKey"`
 	SequenceNumber int    `gorm:"not null"`
